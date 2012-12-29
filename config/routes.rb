80c240d9f0bb35cbcd744d0edb8a670784947387
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-12-29 16:53:40 -0500

    added redirects to ensure that relative links resolve correctly

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Genie::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   resources :lessons
+
+  # add trailing slashes to lessons/jimjh/floating-point so that relative links
+  # for images resolve to jimjh/floating-point/images.
+  match 'lessons/:user/:project' => redirect('/lessons/%{user}/%{project}/'),
+    constraints: lambda { |r| !r.original_fullpath.ends_with? '/' }
   match 'lessons/*path' =>  'lessons#show', via: 'get'
 
   # You can have the root of your site routed with "root"
