b0121f43506eebdba73cec21650d55dec7b69f8d
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-12-29 10:59:36 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Genie::Application.config.secret_token = '9637bd692b1b630dec2940590e773dedaac52d85fcf6053c914c78e2155355ff0b65c4194d5978e05b843b6338375b4fff9eb3c2dd84864cc03800d8268df221'
