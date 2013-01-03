698a8091e571b823a7fb886e0b6baff8b26144e6
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-01-02 23:16:49 -0500

    added validations and specs for auth

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,6 +1,20 @@
 class Authorization < ActiveRecord::Base
 
-  belongs_to :user
   attr_accessible :link, :name, :nickname, :provider, :secret, :token, :uid
+
+  # relationships ------------------------------------------------------------
+  belongs_to :user
+
+  # validations --------------------------------------------------------------
+  validates_presence_of :provider, :uid, :user_id, :token
+  validate              :user_must_exist
+
+  private
+
+  def user_must_exist
+    user_id.nil? || User.find(user_id)
+  rescue ActiveRecord::RecordNotFound
+    errors.add :user, 'is not a registered user.'
+  end
 
 end
