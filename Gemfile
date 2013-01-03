3ecade0e4d5e380f9de8dfe66f218baaeb1a3a01
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-01-03 12:47:15 -0500

    updated rails to 3.2.10

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '~> 3.2.9'
+gem 'rails', '~> 3.2.10'
 gem 'sqlite3'
 
 # Gems used only for assets and not required
@@ -32,6 +32,6 @@ gem 'omniauth',         '~> 1.1.1'
 gem 'omniauth-github',  '~> 1.0.3'
 gem 'uuidtools',        '~> 2.1.3'
 gem 'simple_form',      '~> 2.0.4'
-gem 'friendly_id', '~> 4.0.1'
+gem 'friendly_id',      '~> 4.0.1'
 
 gem 'lamp', git: 'git@github.com:jimjh/genie-worker.git', branch: 'master'
--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,16 @@ Genie::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
 
   match 'new' => 'lessons#new', as: 'new_lesson', via: :get
+  resources :lessons, except: [:new, :show]
 
-  constraints :user => /(?!new|users|devise)/ do
+  # constraints :user => /(?!new|users|devise)/ do
     # add trailing slashes to lessons/jimjh/floating-point so that relative
     #   links for images resolve to jimjh/floating-point/images.
     match ':user/:lesson' => redirect('/%{user}/%{lesson}/'),
       constraints: lambda { |r| !r.original_fullpath.ends_with? '/' }
     match ':user/:lesson/verify/:type/:problem' => 'lessons#verify'
     match ':user/:lesson(/*path)' => 'lessons#show', as: 'lesson'
-  end
-
-  resources :lessons, except: [:new, :show]
+  # end
 
   root :to => 'home#index'
 
