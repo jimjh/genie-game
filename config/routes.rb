8e666ca193b3123fb6f9a53b41591db3cb2a07ea
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-01-07 15:29:11 -0500

    oops - removed one too many devise modules

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Genie::Application.routes.draw do
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   resources :lessons, except: [:new, :show, :index, :edit, :update] do
     post :push, on: :collection,
