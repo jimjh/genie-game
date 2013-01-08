722ec399ac282abf677639f50cad288975da0231
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-01-08 16:23:02 -0500

    updated routes and comments

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,3 +1,4 @@
+# This entire controller operates under the +current_user+ context.
 class SettingsController < ApplicationController
 
   before_filter :authenticate_user!
@@ -9,8 +10,7 @@ class SettingsController < ApplicationController
 
   def index
     auth = current_user.authorizations.find_by_provider('github') || not_found
-    # FIXME:  pagination, error handling
-    @repos     = Github.new.repos.list user: auth.nickname
+    @repos     = Github.new.repos.list(user: auth.nickname, page: params[:page] || 0)
     @published = current_user.lessons.pluck(:url)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Genie::Application.routes.draw do
 
+  # User management w. Devise ------------------------------------------------
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } do
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
-  resources :lessons, except: [:new, :show, :index, :edit, :update] do
+  # Lessons Controller -------------------------------------------------------
+  resources :lessons, only: [:create, :destroy] do
     post :push, on: :collection,
       constraints: lambda { |r| Rails.configuration.github[:ips].include? r.remote_ip }
   end
 
-  match 'settings' => 'settings#index'
+  # Settings Controller ------------------------------------------------------
+  match ':controller(/:action)' => { controller: 'settings' }
+
+  root :to => 'home#index'
+
+  # Pretty URLs --------------------------------------------------------------
 
   # add trailing slashes to lessons/jimjh/floating-point so that relative
   #   links for images resolve to jimjh/floating-point/images.
@@ -17,7 +24,5 @@ Genie::Application.routes.draw do
     via: :get, constraints: lambda { |r| !r.original_fullpath.ends_with? '/' }
   match ':user/:lesson/verify/:type/:problem' => 'lessons#verify', via: :post
   match ':user/:lesson(/*path)' => 'lessons#show', as: 'lesson', via: :get
-
-  root :to => 'home#index'
 
 end
