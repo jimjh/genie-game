f6b5d1e352e3c7a9a301b925a2844bca39ef3b58
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-03-16 11:19:36 -0400

    finally got deploy.rb right

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,6 +37,9 @@ namespace :deploy do
     run "cd #{current_path}; bundle exec rake RAILS_ENV=production db:schema:load"
   end
   task :secrets do
-    run "ln -nfs #{deploy_to}/shared/config/locals.d #{release_path}/config/environments/locals.d"
+    run "ln -nfs #{shared_path}/config/locals.d #{release_path}/config/environments/locals.d"
+  end
+  task :migrate do
+    run "cd #{release_path} && bundle exec rake RAILS_ENV=production --require ./config/environment db:migrate"
   end
 end
