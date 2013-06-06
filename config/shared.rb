308ccdad3527dac0ba0a9c32a5bfdd29f9460136
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-06-05 23:26:52 -0400

    fixed dns settings

--- config/shared.rb
+++ config/shared.rb
@@ -1,7 +1,8 @@
 module Genie
   module SharedConstants
 
-    HOST  = 'ec2-54-225-0-30.compute-1.amazonaws.com'
+    #HOST  = 'ec2-54-225-0-30.compute-1.amazonaws.com'
+    HOST  = 'beta.geniehub.org'
     IP    = '54.225.0.30'
 
     REDIS = { host: 'localhost', port: 6379 }
