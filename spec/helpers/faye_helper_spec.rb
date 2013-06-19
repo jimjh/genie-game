a9e9f9d85d4dc1055b8471ce693924d91b8e58fa
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-06-19 09:00:06 -0400

    [#51408335] fixed specs

--- spec/helpers/faye_helper_spec.rb
+++ spec/helpers/faye_helper_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe FayeHelper do
 
-  describe '#faye' do
+  describe '#faye_url' do
     it 'returns a URI string' do
-      %w[http https].should include URI(helper.faye).scheme
+      %w[http https].should include URI(helper.faye_url).scheme
     end
   end
 
