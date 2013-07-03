d6975fc56ae1ff46b68fcb77d5bf8fb2cdf668be
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2013-07-02 23:27:51 -0400

    fixed migrations

--- db/migrate/20130330160229_convert_digest_to_binary.rb
+++ db/migrate/20130330160229_convert_digest_to_binary.rb
@@ -1,7 +1,7 @@
 class ConvertDigestToBinary < ActiveRecord::Migration
 
   def change
-    change_column :problems, :digest, :binary
+    change_column :problems, :digest, :binary, :limit => 32
   end
 
 end
